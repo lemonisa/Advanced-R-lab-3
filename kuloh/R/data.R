ca81ae834ffee8b18ae31a1f8751bceff3b8f9f4
arianbarakat <arianbarakat@gmail.com>
2016-09-14 22:55:26 +0200

    Added data

---
+++ kuloh/R/data.R
@@ -0,0 +1,10 @@
+#' A Dataset for the wiki_graph
+#'
+#' The wiki_graph is a dataset that has 18 observation and 3 variables.
+#' The name of the variables are "v1","v2" and "w". These repesent the nodes
+#' and the weight of each node.
+#'
+#' The wiki_graph can be used with the dijkstra function.
+#'
+#' @source http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+"wiki_graph"
