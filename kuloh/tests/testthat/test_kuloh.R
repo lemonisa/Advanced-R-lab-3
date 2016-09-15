a2d1fd9af3d66b56c49a4063ae01e88438636970
jackmat <sansguitar@hotmail.com>
2016-09-15 11:44:08 +0200

    written one small test for dkjistra

--- kuloh/tests/testthat/test_kuloh.R
+++ kuloh/tests/testthat/test_kuloh.R
@@ -7,4 +7,19 @@ test_that("The code gives correct output", {
   expect_that(euclidian(2,8), equals(2))
   expect_that(euclidian(3,5), equals(1))
   expect_that(euclidian(123612, 13892347912), equals(4))
+  })
+
+
+
+
+
+
+
+
+context("dijkstra")
+
+test_that("The code gives correct output", {
+  expect_that(dijkstra(wiki_graph, 1), equals(c(0, 7, 9, 20, 20, 11)))
+  expect_that(dijkstra(wiki_graph, 1), equals(c(9, 10, 0, 11, 11, 2)))
 })
+
